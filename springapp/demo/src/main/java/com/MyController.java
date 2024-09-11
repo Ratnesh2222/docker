0e1047e7c686132dac6c9f6f1de83ea90fe89a38
Ratnesh Srivastava <Ratnesh.Srivastava@ibm.com>
2024-09-11 09:57:05 +0530

    spring app

---
+++ springapp/demo/src/main/java/com/MyController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @GetMapping("/get")
+    public String get() {
+        return "hi, i am sprinboot";
+    }
+}
